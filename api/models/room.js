d36853790aa369fc021e43ed80768fefd5825210
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-18 15:54:11 +0500

    added rooms schema

---
+++ api/models/room.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+    roomNo: { type: String, required: true, unique: true },
+    roomType: { type: String, enum: ['Single', 'Double'], required: true },
+    pricePerDay: { type: Number, required: true },
+    roomImage: { type: String },
+    description: { type: String },
+    availabilityStatus: { type: Boolean, default: true },
+    history: [{
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        arrivalDate: { type: Date },
+        departureDate: { type: Date }
+    }]
+});
+
+const Room = mongoose.model('Room', roomSchema);
+module.exports = Room;
