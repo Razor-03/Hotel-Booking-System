036af0269adae7b4010e867739499bb7b5df49ba
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-19 17:59:47 +0500

    added more routes

---
+++ api/routes/users.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import User from "../models/user.schema.js";
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
