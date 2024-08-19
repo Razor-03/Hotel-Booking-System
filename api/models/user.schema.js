fadbe32911b754f1a1f16a28acefa6be81a2c8ee
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-19 15:05:51 +0500

    changed name field

--- api/models/user.schema.js
+++ api/models/user.schema.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     contact: { type: String, required: true },
     arrivalDate: { type: Date, required: true },
