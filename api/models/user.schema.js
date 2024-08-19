fe2ee6ac713b465919904e7bf00c54a0b262c680
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-19 12:40:37 +0500

    added users schema

---
+++ api/models/user.schema.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    contact: { type: String, required: true },
+    arrivalDate: { type: Date, required: true },
+    departureDate: { type: Date, required: true },
+    numberOfChildren: { type: Number, default: 0 },
+    numberOfAdults: { type: Number, required: true },
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
