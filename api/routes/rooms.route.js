33e2b4d1fb00a824821aa54d9a24252e0e17db44
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-19 15:35:10 +0500

    added rooms index route

--- api/routes/rooms.route.js
+++ api/routes/rooms.route.js
@@ -1,8 +1,40 @@
 import express from "express";
+import Room from "../models/room.schema.js";
+import User from "../models/user.schema.js";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json({message: "Hello from rooms route"});
+router.get("/", async (req, res) => {
+    const { roomType, floor, minPrice, maxPrice, availabilityStatus } = req.query;
+
+    let query = {};
+
+    if (minPrice) {
+        query.pricePerNight = { ...query.price, $gte: parseInt(minPrice) };
+    }
+
+    if (maxPrice) {
+        query.pricePerNight = { ...query.price, $lte: parseInt(maxPrice) };
+    }
+
+    if (roomType) {
+        query.roomType = roomType;
+    }
+
+    if (floor) {
+        query.floor = parseInt(floor);
+    }
+
+    if (availabilityStatus) {
+        query.availabilityStatus = availabilityStatus === "true";
+    }
+
+    try {
+        const rooms = await Room.find().populate("history.user");
+        res.status(200).json(rooms);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+
 });
 
 export default router;
