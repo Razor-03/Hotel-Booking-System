40fd05367bdfb23003a6544453f232ecf742880c
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-21 20:14:30 +0500

    removed password field

--- api/models/user.schema.js
+++ api/models/user.schema.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     contact: { type: String, required: true },
     arrivalDate: { type: Date, required: true },
