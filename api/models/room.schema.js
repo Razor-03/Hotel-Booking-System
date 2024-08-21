1c5a95003eebe71b3bee4701bf9553d7b484ebef
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-21 21:01:46 +0500

    added servant fields

--- api/models/room.schema.js
+++ api/models/room.schema.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const roomSchema = new Schema({
     roomNo: { type: String, required: true, unique: true },
     roomType: { type: String, enum: ['Single', 'Double'], required: true },
+    servantName: { type: String, required: true },
+    servantContact: { type: String, required: true },
     floor: { type: Number, required: true },
     pricePerNight: { type: Number, required: true },
     roomImage: { type: String },
