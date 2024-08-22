fb3aa548c22305e80013830cc510e8d797f63ff9
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-22 16:37:25 +0500

    added more fields

--- api/models/user.schema.js
+++ api/models/user.schema.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'user'], default: 'user' },
     email: { type: String, required: true, unique: true },
     contact: { type: String, required: true },
     arrivalDate: { type: Date, required: true },
