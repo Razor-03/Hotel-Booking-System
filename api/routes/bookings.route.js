4b04f9faa67e83302abdc5ae026dea6446e4171b
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-22 14:30:44 +0500

    added reject or approve route

---
+++ api/routes/bookings.route.js
@@ -0,0 +1,64 @@
+import express from "express";
+import Room from "../models/room.schema.js";
+import User from "../models/user.schema.js";
+import Booking from "../models/booking.schema.js";
+import Review from "../models/review.schema.js";
+const router = express.Router();
+
+router.put('/:id/approve', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const booking = await Booking.findById(id);
+        if (!booking) {
+            return res.status(404).json({ error: 'Booking not found.' });
+        }
+
+        if (booking.status === 'Approved') {
+            return res.status(400).json({ error: 'Booking is already approved.' });
+        }
+
+        booking.bookingStatus = 'Approved';
+
+        await booking.save();
+
+        res.status(200).json({
+            message: 'Booking approved successfully!',
+            id: booking._id
+        });
+
+    } catch (error) {
+        console.error('Error approving booking:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.put('/:id/reject', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const booking = await Booking.findById(id);
+        if (!booking) {
+            return res.status(404).json({ error: 'Booking not found.' });
+        }
+
+        if (booking.status === 'Rejected') {
+            return res.status(400).json({ error: 'Booking is already rejected.' });
+        }
+
+        booking.status = 'Rejected';
+
+        await booking.save();
+
+        res.status(200).json({
+            message: 'Booking rejected successfully!',
+            id: booking._id
+        });
+
+    } catch (error) {
+        console.error('Error rejecting booking:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+export default router;
