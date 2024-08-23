67e5962dc735ff16573e826f4f90257806894637
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-23 17:10:42 +0500

    styled rooms list

--- client/src/routes/RoomsListPage.jsx
+++ client/src/routes/RoomsListPage.jsx
@@ -6,26 +6,28 @@ import { Suspense } from "react";
 export default function RoomsListPage() {
   const data = useLoaderData();
   return (
-    // <div className="flex h-full overflow-hidden">
+    <div className="flex h-full">
       <div className="lg:pr-24 flex flex-col gap-20 h-full">
         <Filter />
-        <Suspense fallback={<div className="">Loading...</div>}>
-          <Await
-            resolve={data.postResponse}
-            errorElement={
-              <div className="font-bold text-red-500">
-                Error loading posts...
-              </div>
-            }
-          >
-            {(postResponse) =>
-              postResponse.data.map((item) => (
-                <Card key={postResponse.id} item={item} />
-              ))
-            }
-          </Await>
-        </Suspense>
+        <div className="flex gap-12 lg:gap-32 flex-wrap justify-center">
+          <Suspense fallback={<div className="">Loading...</div>}>
+            <Await
+              resolve={data.postResponse}
+              errorElement={
+                <div className="font-bold text-red-500">
+                  Error loading posts...
+                </div>
+              }
+            >
+              {(postResponse) =>
+                postResponse.data.map((item) => (
+                  <Card key={postResponse._id} item={item} />
+                ))
+              }
+            </Await>
+          </Suspense>
+        </div>
       </div>
-    // </div>
+    </div>
   );
 }
