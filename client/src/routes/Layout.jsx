24843e7ef87ed4c210f458c318bf03e680ac6f7f
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-23 15:44:25 +0500

    created layout for app

---
+++ client/src/routes/Layout.jsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import Navbar from "../components/Navbar";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+export function Layout() {
+    return (
+        <div className="flex flex-col max-w-[100%] md:max-w-[90%] xl:max-w-[90%] h-screen mx-auto px-[20px] text-[#0d1321] oveflow-x-hidden">
+            <div className="">
+                <Navbar />
+            </div>
+            <div className="h-fit overflow-hidden">
+                <Outlet />
+            </div>
+        </div>
+    );
+}
+
+export function RequireAuth() {
+    const { currentUser } = useContext(AuthContext);
+    
+    return (
+        !currentUser ? <Navigate to="/login" /> :
+            <div className="flex flex-col max-w-[100%] md:max-w-[90%] xl:max-w-[90%] h-screen mx-auto px-[20px] text-[#0d1321] oveflow-x-hidden">
+                <div className="">
+                    <Navbar />
+                </div>
+                <div className="h-screen">
+                    <Outlet />
+                </div>
+            </div>
+    );
+}
