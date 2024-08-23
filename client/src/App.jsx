dd6a04eb55fc0ce36c418bd0eae886b5d3d22aa6
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-08-23 15:46:24 +0500

    created router for app routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,62 @@
-import './App.css'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./routes/Layout";
+import Homepage from "./routes/Homepage";
+import { roomsListLoader } from "./lib/loaders";
+import RoomsListPage from "./routes/RoomsListPage";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Homepage />,
+        },
+        {
+          path: "/list",
+          element: <RoomsListPage />,
+          loader: roomsListLoader,
+        },
+        // {
+        //   path: "/login",
+        //   element: <Login />,
+        // },
+        // {
+        //   path: "/register",
+        //   element: <Register />,
+        // },
+      ],
+    },
+    // {
+    //   path: "/",
+    //   element: <RequireAuth />,
+    //   children: [
+    //     {
+    //       path: "/profile",
+    //       element: <ProfilePage />,
+    //       loader: profilePageLoader,
+    //     },
+    //     {
+    //       path: "/:id",
+    //       element: <Singlepage />,
+    //       loader: singlePageLoader,
+    //     },
+    //     {
+    //       path: "/profile/update",
+    //       element: <ProfileUpdatePage />,
+    //     },
+    //     {
+    //       path: "/add",
+    //       element: <NewPostPage />,
+    //     },
+    //   ],
+    // },
+  ]);
 
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
