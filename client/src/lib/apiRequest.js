6a4d4011365b27fe9a39c78fb281ec1ce91246f9
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-09-09 23:34:21 +0500

    change baseURL o production

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://marred-global-cirrus.glitch.me/api",
     withCredentials: true,
 });
 
